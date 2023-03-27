b2e7ffc561afa57fcbf67753ea4c698bec34c521
Todeschini.19331 <Todeschini.19331@LABI1-6PC23>
2023-03-27 10:48:29 +0200

    Task 5

--- TodeT_InfoTest/Form1.cs
+++ TodeT_InfoTest/Form1.cs
@@ -17,5 +17,40 @@ namespace TodeT_InfoTest
             InitializeComponent();
         }
 
+        //STRUTTURA DINAMICA
+        public List<TodeT_Candidato> lista = new List<TodeT_Candidato>();
+
+        //AGGIUNTA
+        public void Aggiunta(TodeT_Candidato a)
+        {
+            lista.Add(a);
+        }
+
+        //VISUALIZZA
+        public string[] Visualizza(List<TodeT_Candidato> a)
+        {
+            string[] s = new string[999];
+            int n = 0;
+            foreach(TodeT_Candidato c in lista)
+            {
+                s[n] = c.ToString();
+                n++;
+            }
+
+            return s;   
+        }
+
+        //MODIFICA
+        public void Modifica(int nmatricola, string nome)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Matricola == nmatricola)
+                {
+                    lista[i].Nome = nome;
+                }
+            }
+        }
+
     }
 }
