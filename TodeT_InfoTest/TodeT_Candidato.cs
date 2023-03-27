3d056954af1e6e1df6826b120090fe4d53a01032
Todeschini.19331 <Todeschini.19331@LABI1-6PC23>
2023-03-27 08:37:53 +0200

    Task 1 Completa

--- TodeT_InfoTest/TodeT_Candidato.cs
+++ TodeT_InfoTest/TodeT_Candidato.cs
@@ -8,8 +8,58 @@ namespace TodeT_InfoTest
 {
     internal class TodeT_Candidato
     {
-        private int TodeT_matricola;
+        private int _TodeT_matricola;
 
-        private string TodeT_nome;
+        private string _TodeT_nome;
+
+        //PROPERTIES
+        public int Matricola
+        {
+            get { return _TodeT_matricola; }
+            set { _TodeT_matricola = value; }
+        }
+        public string Nome
+        {
+            get { return _TodeT_nome; }
+            set { _TodeT_nome = value; }
+        }
+
+        //OOSTRUTTORE SENZA PARAMETRI
+        public TodeT_Candidato()
+        {
+            Matricola = 0;
+            Nome = null;
+        }
+
+        //COSTRUTTORE CON UN PARAMETRO
+        public TodeT_Candidato(int matricola)
+        {
+            Matricola = matricola;
+            Nome=null;
+        }
+        public TodeT_Candidato(string nome)
+        {
+            Matricola = 0;
+            Nome = nome;
+        }
+
+        //COSTRUTTORE CON DUE PARAMETRI
+        public TodeT_Candidato(int matricola, string nome)
+        {
+            Matricola=matricola;
+            Nome = nome;
+        }
+
+        //METODO ASTRATTO ISIDONEO
+        public static bool isIdoneo(bool a)
+        {
+            return a;
+        }
+
+        //METODO ASTRATTO PUNTEGGIO
+        public static int punteggio(int i)
+        {
+            return i;
+        }
     }
 }
